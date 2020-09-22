2dfa075950d2820f00c4de71b9eda0e56459325b
daryabsb <daryabsb@gmail.com>
2020-09-22 09:28:07 +0300

    Adding the new UI

---
+++ core/managers.py
@@ -0,0 +1,10 @@
+# from django.db import models
+# from .models import Journal
+
+
+# class JournalQueryset(models.QuerySet):
+#     pass
+
+# class JournalManager(models.Manager):
+
+#     def get_queryset(self.QuerySet)
